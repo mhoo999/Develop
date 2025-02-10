d75a078c215c71096fcdb2d9c99d379afc11aed6
myeonghoon <mhoo999@naver.com>
2025-02-10 14:27:08 +0900

    init

---
+++ src/main/java/com/example/develop/DevelopApplication.java
@@ -0,0 +1,15 @@
+package com.example.develop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@SpringBootApplication
+public class DevelopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevelopApplication.class, args);
+    }
+
+}
